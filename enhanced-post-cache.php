0ac2f531c5e21b3b38d680940a29db1794615c03
Jonny Harris <jon@spacedmonkey.co.uk>
2017-11-02 23:35:28 +0000

    Invalid post type caches when term is added / editted.

--- enhanced-post-cache.php
+++ enhanced-post-cache.php
@@ -25,7 +25,7 @@ class Enhanced_Post_Cache {
 
 		add_action( 'switch_blog', array( $this, 'setup_for_blog' ), 10, 2 );
 
-		add_action( 'clean_term_cache', array( $this, 'flush_cache' ) );
+		add_action( 'clean_term_cache',  array( $this, 'clean_term_cache' ), 10, 2 );
 		add_action( 'clean_post_cache',  array( $this, 'clean_post_cache' ), 10, 2 );
 
 		add_action( 'deleted_post_meta', array( $this, 'update_post_meta' ), 10, 2 );
@@ -62,9 +62,24 @@ class Enhanced_Post_Cache {
 		}
 	}
 
+<<<<<<< HEAD
 	public function update_post_meta( $ignored, $post_id ) {
 		$post = get_post( $post_id );
 		$this->clean_post_cache( $post_id, $post );
+=======
+	public function clean_term_cache( $ids, $taxonomy_name ) {
+		$taxonomy = get_taxonomy( $taxonomy_name );
+		if ( $taxonomy ) {
+			foreach ( $taxonomy->object_type as $post_type ) {
+				if ( post_type_exists( $post_type ) ) {
+					$this->cache_salt_key = $post_type;
+					$this->flush_cache();
+				}
+			}
+		} else {
+			$this->flush_cache();
+		}
+>>>>>>> Invalid post type caches when term is added / editted.
 	}
 
 	public function flush_cache() {
